493e1e8fe7bfedc19016e2ab5c2cfc1e04b032bf
ajeetach97701 <ajeetacharya02@gmail.com>
2024-09-30 18:01:02 +0545

    test

--- Agent/agent_supervisor_take_2.py
+++ Agent/agent_supervisor_take_2.py
@@ -1,64 +1,125 @@
+from Libs.libs import llm
 from langchain_core.tools import tool
-
+from Libs.libs import *
 from Tools.Schema import DateModel
 from typing   import Literal
 import pandas as pd
 
 @tool
-def check_availability_by_doctor():
+def check_availability_by_doctor(desired_date:str, doctor_name:Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
     # desired_date, doctor_name:Literal['kevin anderson','Ajeet','Rajesh','Shiva','Ganesh',]):
     """
     Check the availability of the doctor with the name and date provided
     """
-    doctor_name = "kevin anderson" 
-    desired_date = "2024-09-03"
-    print("Tool entered")
+    # print(desired_date)
+    desired_date_split = desired_date.split("T") or desired_date
+    print("desired date is:",desired_date)
     df = pd.read_csv(f"./data/syntetic_data/availability.csv")
-    availability = df[(df['date_slot'].apply(lambda x: x.split(' ')[0]) == desired_date) & (doctor_name == df['doctor_name'])&(df['is_available'] == True)]["date_slot"]
-    outpur = ""
-    if len(availability) == 0:
-        outpur = "Doctor not available for the day"
-    else:
-        outpur = f"Availablity for the doctor {doctor_name}is+ ""+{availability}".join(availability)
-    return outpur
+    date =  desired_date_split[0] 
+    time = desired_date_split[1] if len(desired_date_split) > 1 else "" 
+    # query = doctor_name+ " for " + date + " at time: " +time
+    query = doctor_name+ " for " + date + "," +time
 
-from Libs.libs import llm
+    print()
+    print()
+    print("Tool entered with the query", query)
+    print()
+    print()
+    availability = df[(df['date_slot'].apply(lambda x: x.split(' ')[0]) == date) & (doctor_name == df['doctor_name'])&(df['is_available'] == True)]
+    # print(availability)
+    template = """
+    You have to answer the user query which is about the availability of a doctor inquired by patient. You are also given a context that contains the doctors available in that day and a time slot. Analyze and return the doctor name, date and available time slot. 
+        Context inside double backticks:`{context}`
+        Question inside triple backticks:{question}
+        Response in markdown format without any backticks and in the context phrase just answer what is asked and if the doctor is not available for a particular time slot, recomend the other time slots.
 
-@tool
-def mercedes_tool(text:str):
-    """
-    A tool that is used to answer all the queries related to mercedes.
-    """
-    # df = pd.read_csv(f"./data/syntetic_data/availability.csv")
-    # rows = df[(df['date_slot'].apply(lambda input: input.split(' ')[0]) == desired_date) & (df['specialization'] == specialization) & (df['is_available'] == True)].reset_index()
-    # available_specialization = rows[['date_slot', 'specialization', 'doctor_name']]    
-    # if len(available_specialization) == 0:
-    #     return f"No doctors are available for {available_specialization}"    
-    # else:
-    #     output = "availability for the doctor"
-    #     for index, row in available_specialization.iterrows():
-    #         output += f"\n{row['date_slot']}"
+        Your response should be very much conversational in such a way that you are a receptionist talking to a patient who is here to schedule an appointment.
+        """
+    prompt = ChatPromptTemplate.from_template(template)
+    # print("context>>>>>>>>>>>>>>>>",database.similarity_search(query,k=5))
+    chain = RunnableMap({
+        "context":lambda x: availability,
+        "question": lambda x: x['question']
+    }) | prompt | llm | string_parser
+    result = chain.invoke({'question':query})
+    print("------------")
+    mdprint(result)
+    print("------------")
+    return {"messages":result} 
     
-    return llm.invoke(text)
+    
+    # outpur = ""
+    # if len(availability) == 0:
+    #     outpur = "Doctor not available for the day"
+    #     return llm.invoke(input=f"Your should return this text in a markdown format in a good language No doctors are available for {availability}"  )
+    # else:
+    #     outpur = f"Availablity for the doctor {doctor_name}is+ ""+{availability}".join(availability)
+    #     return llm.invoke(input=f"Your should return this text in a markdown format in a good language No doctors are available for {outpur}"  )
+
+    # return outpur
+
 
 
 @tool
-def check_availability_by_specialization():
+def check_availability_by_specialization(desired_date:str, specialization:Literal["general_dentist", "cosmetic_dentist", "prosthodontist", "pediatric_dentist","emergency_dentist","oral_surgeon","orthodontist"]):
     """
     Check the availability of the doctor with specialization and date provided.
     """
-    specialization = "orthodontist"
-    desired_date = "2024-09-03"
+    # specialization = "orthodontist"
+    # desired_date = "2024-09-03"
+    desired_date_split = desired_date.split("T") or desired_date
+
+    date =  desired_date_split[0] 
+    time = desired_date_split[1] if len(desired_date_split) > 1 else "" 
     df = pd.read_csv(f"./data/syntetic_data/availability.csv")
-    rows = df[(df['date_slot'].apply(lambda input: input.split(' ')[0]) == desired_date) & (df['specialization'] == specialization) & (df['is_available'] == True)].reset_index()
-    available_specialization = rows[['date_slot', 'specialization', 'doctor_name']]    
-    if len(available_specialization) == 0:
-        return f"No doctors are available for {available_specialization}"    
-    else:
-        output = "availability for the doctor"
-        for index, row in available_specialization.iterrows():
-            output += f"\n{row['date_slot']}"
-        
-        return output
+    rows = df[(df['date_slot'].apply(lambda input: input.split(' ')[0]) == date) & (df['specialization'] == specialization) & (df['is_available'] == True)].reset_index()
+    available_specialization = rows[['date_slot', 'specialization', 'doctor_name']] 
+      
+    query = specialization+ " for " + date + "," +time
+    print()
+    print()
+    print("Tool entered with the query", query)
+    print()
+    print()
+
+    # print(availability)
+    template = """
+    You have to answer the user query in which patient is searching for a doctor of a particular specialization which is given in the query. You are also given a context that contains the doctors available in that day and a time slot. Analyze and return the doctor name, date and available time slot along with the specialization. 
+        Context inside double backticks:`{context}`
+        Question inside triple backticks:{question}
+    Response in markdown format without any backticks and in the context phrase just answer what is asked and if the doctor is not available for a particular time slot, recomend the other time slots.
+    Your response should be very much conversational in such a way that you are a receptionist talking to a patient who is here to schedule an appointment.
+
+        """
+    prompt = ChatPromptTemplate.from_template(template)
+    # print("context>>>>>>>>>>>>>>>>",database.similarity_search(query,k=5))
+    chain = RunnableMap({
+        "context":lambda x: available_specialization,
+        "question": lambda x: x['question']
+    }) | prompt | llm | string_parser
+    result = chain.invoke({'question':query})
+    mdprint(result)
+    return {"messages":result} 
+   
     
+
+@tool
+def book_appointment(patient_name:str,desired_date:str, doctor_name:str, specialization:str, desired_time:str):
+    """
+    To schedule an appointment for a particular date, time.
+    """
+    print("Booking appointment tool")
+    app = patient_name + " " + desired_date + " "+ doctor_name + " " + specialization + " " + desired_time
+    print(app)
+    
+    return app
+
+
+@tool
+def reschedule_appointment(patient_name:str,patient_id:str,desired_date:str, doctor_name:str, specialization:str, desired_time:str):
+    """
+    To reschedule an appointment which has already been scheduled to a new date
+    """
+    pass
+
 
--- Agent/agent_take2.py
+++ Agent/agent_take2.py
@@ -18,7 +18,7 @@ from Libs.libs import *
 class MessagesState(TypedDict):
     messages: Annotated[List[AnyMessage], operator.add]
 
-tools = [check_availability_by_doctor, check_availability_by_specialization]
+tools = [check_availability_by_doctor, check_availability_by_specialization, book_appointment]
 # tool = [mercedes_tool]
 tool_node = ToolNode(tools=tools)
 model = llm.bind_tools(tools = tools, strict=True)
@@ -28,10 +28,20 @@ def read_human_feedback(state):
     #     logger.info("AI: "+ state['messages'][-1].content)
     #     user_msg = input("Reply: ")
     print("..........",state['messages'][-1].content)
-    return state['messages'][-1].content
+    print("..........",state['messages'][0].content)
+    
+    history = {"human":f"{state['messages'][0].content}", "ai":f"{state['messages'][-1].content}"}
+    print()
+    print()
+    print(history)
+    print()
+    print()
+    return state
     #     pass
 
 def call_model(state: MessagesState):
+    # history = {"human":state["messages"][1], "AI":"This is ai response"}
+    print("this is state>>>>>",state)
     messages = [SystemMessage(content=f"You are helpful assistant.\nAs reference, today is {datetime.now().strftime('%Y-%m-%d %H:%M, %A')}.\nKeep a friendly, professional tone.\nAvoid verbosity.\nConsiderations:\n- Don´t assume parameters in call functions that it didnt say.\n- MUST NOT force users how to write. Let them write in the way they want.\n- The conversation should be very natural like a secretary talking with a client.\n- Call only ONE tool at a time.")] + state['messages']
     # messages = [SystemMessage(content="You are a helpful assistant. As reference, today is {datetime.now().strftime('%Y-%m-%d %H:%M, %A')}. Always use tools to answer the queries")]
     response = model.invoke(messages)
@@ -44,7 +54,10 @@ def should_continue(state: MessagesState) -> Literal["tools", "human"]:
         return "tools"
     return "human"
 
-def should_continue_with_feedback(state: MessagesState) -> Literal["agent", "end"]:
+
+
+
+def should_continue_with_feedback(state: MessagesState) -> Literal["agent", "end", "human"]:
     messages = state['messages']
     last_message = messages[-1]
     if isinstance(last_message, dict):
@@ -52,6 +65,8 @@ def should_continue_with_feedback(state: MessagesState) -> Literal["agent", "end
             return "agent"
     if (isinstance(last_message, HumanMessage)):
         return "agent"
+    if (isinstance(last_message, AIMessage)):
+        return "end"
     return "end"
 
 workflow = StateGraph(MessagesState)
@@ -72,7 +87,7 @@ workflow.add_conditional_edges(
     {"agent":"agent","end":END}
 )
 
-workflow.add_edge("agent", "tools")
+workflow.add_edge("tools","agent" )
 
 
 workflow.set_entry_point('agent')
